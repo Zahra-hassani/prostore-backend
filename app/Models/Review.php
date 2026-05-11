29ce563aa0d120238661461eef42399dc40aacf7
Zahra-hassani <hassanizahra897@gmail.com>
2026-05-12 03:59:55 +0430

    function index added for reviews

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -16,7 +16,7 @@ class Review extends Model
         return $this->belongsTo(Product::class,'product_id');
     }
 
-    public function users(){
+    public function user(){
         return $this->belongsTo(User::class);
     }
 }
