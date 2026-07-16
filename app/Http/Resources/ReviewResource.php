a60566bdb5c9bdf561f9d9563e2d6845ecd8d137
Zahra-hassani <hassanizahra897@gmail.com>
2026-07-16 13:40:28 +0430

    new route for reviews in dashboard

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -15,6 +15,7 @@ class ReviewResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            "id" => $this->id,
             "product_id" => $this->product->id,
             "product_name" => $this->product->name,
             "user_name" => $this->user->name,
